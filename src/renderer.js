127cc7bb69f16243f2cba299924da2ae956e80b6
htt1230 <youtan12230@gmail.com>
2025-10-18 13:53:17 +0900

    rotate

--- src/renderer.js
+++ src/renderer.js
@@ -1,4 +1,4 @@
-var index = 0;
+var recording = false;
 
 // HTMLドキュメントの読み込みが完了したら、以下の処理を実行する
 document.addEventListener('DOMContentLoaded', () => {
@@ -6,9 +6,20 @@ document.addEventListener('DOMContentLoaded', () => {
   // 「開始」ボタンの要素(Element)をIDで取得します。
   const startButton = document.getElementById('btn-start');
   startButton.addEventListener('click', async () => {
-    console.log('Renderer: 録画開始ボタンがクリックされました。');
-    startButton.innerText += `w`;
-    index++;
+
+    if (recording) {
+      startButton.innerText = `録画開始`;
+      startButton.style.transition = `all 0.3s`;
+      startButton.style.backgroundColor = `#3b82f6`; // 青色に戻す
+      startButton.style.rotate = `0deg`;
+      recording = false;
+    } else {
+      startButton.innerText = `録画停止`;
+      startButton.style.transition = `all 0.3s`;
+      startButton.style.backgroundColor = `#ef4444`; // 赤色に変更
+      startButton.style.rotate = `360deg`;
+      recording = true;
+    }
   });
 
 });
