ddf6c4add6a57befef9b024f20be24882bde2a47
htt1230 <youtan12230@gmail.com>
2025-10-18 13:27:46 +0900

    humorを追加

--- src/renderer.js
+++ src/renderer.js
@@ -1,38 +1,14 @@
-// HTMLの各要素(Element)をIDで取得します。
-const intervalInput = document.getElementById('interval');
-const resolutionSelect = document.getElementById('resolution');
-const startButton = document.getElementById('btn-start');
-const stopButton = document.getElementById('btn-stop');
-const statusText = document.getElementById('status-text'); // ステータス表示用のspan要素
+var index = 0;
 
-// 「開始」ボタンがクリックされた時の処理
-startButton.addEventListener('click', async () => {
-  const settings = {
-    interval: intervalInput.value,
-    resolution: resolutionSelect.value,
-  };
-  startButton.document = "うんち";
+// HTMLドキュメントの読み込みが完了したら、以下の処理を実行する
+document.addEventListener('DOMContentLoaded', () => {
 
-  console.log('UI -> Mainへ録画開始をリクエスト:', settings);
-  statusText.textContent = 'バックエンドに開始リクエストを送信中...';
+  // 「開始」ボタンの要素(Element)をIDで取得します。
+  const startButton = document.getElementById('btn-start');
+  startButton.addEventListener('click', async () => {
+    console.log('Renderer: 録画開始ボタンがクリックされました。');
+    startButton.innerText += `w`;
+    index++;
+  });
 
-  // preload.js経由で、メインプロセスに処理を依頼します。
-  const response = await window.api.startRecording(settings);
-  console.log('Main -> UIへ応答を受信:', response);
-  
-  // バックエンドからの応答メッセージを画面に表示します。
-  statusText.textContent = `バックエンドからの応答: ${response.message}`;
-});
-
-// 「停止」ボタンがクリックされた時の処理
-stopButton.addEventListener('click', async () => {
-  console.log('UI -> Mainへ録画停止をリクエスト');
-  statusText.textContent = 'バックエンドに停止リクエストを送信中...';
-
-  // preload.js経由で、メインプロセスに処理を依頼します。
-  const response = await window.api.stopRecording();
-  console.log('Main -> UIへ応答を受信:', response);
-
-  // バックエンドからの応答メッセージを画面に表示します。
-  statusText.textContent = `バックエンドからの応答: ${response.message}`;
 });
